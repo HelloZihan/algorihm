8631d62bd8e98b399bb7d496f77c7af7649e9c97
zj <565405576@qq.com>
2021-06-06 23:57:50 +0800

    二叉树的最近公共祖先

---
+++ src/com/leetcode/hot100/LowestCommonAncestor.java
@@ -0,0 +1,57 @@
+package com.leetcode.hot100;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author zhaojun
+ */
+public class LowestCommonAncestor {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        LowestCommonAncestor l = new LowestCommonAncestor();
+        LowestCommonAncestor.TreeNode t = l.new TreeNode(0);
+        t.left = l.new TreeNode(1);
+        t.left.left = l.new TreeNode(2);
+        t.left.right = l.new TreeNode(3);
+        l.lowestCommonAncestor(t, t.left.left, t.left.right);
+    }
+}
