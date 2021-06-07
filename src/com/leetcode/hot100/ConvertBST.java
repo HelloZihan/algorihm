9e85da997c9b3859dae0c314a9d4b007fcdd6a8c
zj <565405576@qq.com>
2021-06-07 23:47:21 +0800

    二叉搜索树转换为累加树

---
+++ src/com/leetcode/hot100/ConvertBST.java
@@ -0,0 +1,55 @@
+package com.leetcode.hot100;
+
+/**
+ * 把二叉搜索树转换为累加树
+ *
+ * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点node的新值等于原树中大于或等于node.val的值之和。
+ *
+ * 提醒一下，二叉搜索树满足下列约束条件：
+ * 1、节点的左子树仅包含键 小于 节点键的节点。
+ * 2、节点的右子树仅包含键 大于 节点键的节点。
+ * 3、左右子树也必须是二叉搜索树。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/convert-bst-to-greater-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class ConvertBST {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int pre = 0;
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        convertBST(root.right);
+        root.val = root.val + pre;
+        pre = root.val;
+        convertBST(root.left);
+    }
+}
