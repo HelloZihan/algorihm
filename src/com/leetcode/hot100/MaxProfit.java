85fa5e1be23fcc6e6b4c0a31b4dc14a550ab5c58
zj <565405576@qq.com>
2021-06-12 20:55:49 +0800

    买卖股票的最佳时机

---
+++ src/com/leetcode/hot100/MaxProfit.java
@@ -0,0 +1,28 @@
+package com.leetcode.hot100;
+
+/**
+ * 121. 买卖股票的最佳时机
+ *
+ * 给定一个数组 prices ，它的第i个元素prices[i] 表示一支给定股票第 i 天的价格。
+ *
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ *
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            result = Math.max(prices[i]-min, result);
+            min = Math.min(min, prices[i]);
+        }
+        return result;
+    }
+}
