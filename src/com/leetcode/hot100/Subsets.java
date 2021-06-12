9647647d247aa8ae02d19b9352fb6ca4ee47e55c
zj <565405576@qq.com>
2021-06-12 20:33:51 +0800

    子集

---
+++ src/com/leetcode/hot100/Subsets.java
@@ -0,0 +1,39 @@
+package com.leetcode.hot100;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 78. 子集
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ *
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ *
+ * @author zhaojun
+ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        result.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int size = result.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> tmp = new ArrayList<>(result.get(j));
+                tmp.add(nums[i]);
+                result.add(tmp);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(new Subsets().subsets(nums));
+    }
+}
