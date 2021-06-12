28bee2cbab67cb6ff4e2fd7218635d25fab69401
zj <565405576@qq.com>
2021-06-12 16:36:23 +0800

    颜色分类

---
+++ src/com/leetcode/hot100/SortColors.java
@@ -0,0 +1,50 @@
+package com.leetcode.hot100;
+
+/**
+ * 75. 颜色分类
+ *
+ * 给定一个包含红色、白色和蓝色，一共n个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ *
+ * 此题中，我们使用整数 0、1 和 2 分别表示红色、白色和蓝色。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,0,2,1,1,0]
+ * 输出：[0,0,1,1,2,2]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sort-colors
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class SortColors {
+    /**
+     * 进阶：
+     *  1、你可以不使用代码库中的排序函数来解决这道题吗？
+     *  2、你能想出一个仅使用常数空间的一趟扫描算法吗？
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        //ptr记住0的位置，每次把其余数放在0那里
+        int ptr = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+        //再把1放在0之后
+        for (int i = ptr; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+    }
+}
