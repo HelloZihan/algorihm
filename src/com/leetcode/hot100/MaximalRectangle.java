c0abc8a2a036145fa052835345ce4d1208e8ba51
zj <565405576@qq.com>
2021-06-13 18:23:09 +0800

    柱状图中最大的矩形(单调栈)

---
+++ src/com/leetcode/hot100/MaximalRectangle.java
@@ -0,0 +1,18 @@
+package com.leetcode.hot100;
+
+/**
+ *  85. 最大矩形
+ * 给定一个仅包含0和1、大小为rows x cols的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ *
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：6
+ * 解释：最大矩形如上图所示。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-rectangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class MaximalRectangle {
+}
