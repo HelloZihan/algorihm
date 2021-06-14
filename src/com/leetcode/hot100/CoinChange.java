062f37f21fccdf95da6133fdb4f57cc4564c422b
zj <565405576@qq.com>
2021-06-14 18:09:25 +0800

    零钱兑换

---
+++ src/com/leetcode/hot100/CoinChange.java
@@ -0,0 +1,44 @@
+package com.leetcode.hot100;
+
+/**
+ * 322. 零钱兑换
+ *
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ *
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ * 示例1：
+ *
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
+ *
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author zhaojun
+ */
+public class CoinChange {
+    /**
+     * 与单词拆分很相似(WordBreak)
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        for(int i=1; i<=amount; i++) {
+            dp[i] = amount+1;
+            for(int j=0; j<coins.length; j++) {
+                if(coins[j] <= i) {
+                    dp[i] = Math.min(dp[i-coins[j]] + 1, dp[i]);
+                }
+            }
+        }
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
+}
