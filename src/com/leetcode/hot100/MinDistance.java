318f370189b6de9481034968ec7b7a513fdace2e
zj <565405576@qq.com>
2021-06-14 14:19:22 +0800

    单词转换最少操作(dp)

---
+++ src/com/leetcode/hot100/MinDistance.java
@@ -0,0 +1,76 @@
+package com.leetcode.hot100;
+
+/**
+ * 72. 编辑距离
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ *
+ * 你可以对一个单词进行如下三种操作：
+ *
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ *
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/edit-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class MinDistance {
+
+    /**
+     *
+     * 思路
+     * 1、动态规划
+     *
+     * 2、定义 dp[i][j]
+     *  1. dp[i][j] 代表 word1 中前 i 个字符，变换到 word2 中前 j 个字符，最短需要操作的次数
+     *  2. 需要考虑 word1 或 word2 一个字母都没有，即全增加/删除的情况，所以预留 dp[0][j] 和 dp[i][0]
+     *
+     * 3、状态转移
+     *  1. 增，dp[i][j] = dp[i][j - 1] + 1
+     *  2. 删，dp[i][j] = dp[i - 1][j] + 1
+     *  3. 改，dp[i][j] = dp[i - 1][j - 1] + 1
+     *  4. 按顺序计算，当计算 dp[i][j] 时，dp[i - 1][j] ， dp[i][j - 1] ， dp[i - 1][j - 1] 均已经确定了
+     *  5. 配合增删改这三种操作，需要对应的 dp 把操作次数加一，取三种的最小
+     *  6. 如果刚好这两个字母相同 word1[i - 1] = word2[j - 1] ，那么可以直接参考 dp[i - 1][j - 1] ，操作不用加一
+     *
+     * 作者：ikaruga
+     * 链接：https://leetcode-cn.com/problems/edit-distance/solution/edit-distance-by-ikaruga/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        //dp[i][j] 代表 word1 中前 i 个字符，变换到 word2 中前 j 个字符，最短需要操作的次数
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 0; i < n+1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < m+1; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
