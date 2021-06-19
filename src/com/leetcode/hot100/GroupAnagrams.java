b8d70f2bce2a10f4dbde90c092407497697ace8c
zj <565405576@qq.com>
2021-06-19 12:26:46 +0800

    字母异位词分组(map)

---
+++ src/com/leetcode/hot100/GroupAnagrams.java
@@ -0,0 +1,38 @@
+package com.leetcode.hot100;
+
+import java.util.*;
+
+/**
+ * 49. 字母异位词分组
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String s = new String(chars);
+            List<String> l = map.getOrDefault(s, new ArrayList<>());
+            l.add(strs[i]);
+            map.put(s, l);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
