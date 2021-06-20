9f9ad4d8fad898dafe5dee8954c1215bd5f8a4b9
zj <565405576@qq.com>
2021-06-20 14:23:03 +0800

    lru

---
+++ src/com/leetcode/hot100/LRUCache.java
@@ -0,0 +1,133 @@
+package com.leetcode.hot100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 146. LRU 缓存机制
+ *
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+ * 实现 LRUCache 类：
+ *
+ * LRUCache(int capacity) 以正整数作为容量capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value)如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
+ * 当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ *
+ *
+ * 进阶：你是否可以在O(1) 时间复杂度内完成这两种操作？
+ *
+ * 示例：
+ *
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ *
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lru-cache
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class LRUCache {
+
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+        public DLinkedNode() {}
+        public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
+    }
+
+    private Map<Integer, DLinkedNode> map;
+    //记录容量大小
+    private int capacity;
+    //记录元素个数
+    private int size;
+    private DLinkedNode head, tail;
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache obj = new LRUCache(capacity);
+     * int param_1 = obj.get(key);
+     * obj.put(key,value);
+     */
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        map = new HashMap<>(capacity);
+        // 使用伪头部和伪尾部节点
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        //此处head的next为tail，tail的prev为head是预防新增第一个元素时addToHead里的空指针
+        head.next = tail;
+        tail.prev = head;
+
+    }
+
+    public int get(int key) {
+        DLinkedNode dLinkedNode = map.get(key);
+        if (dLinkedNode == null) {
+            return -1;
+        }
+        moveToHead(dLinkedNode);
+        return dLinkedNode.value;
+    }
+
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = map.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            if (size == capacity) {
+                // 如果超出容量，删除双向链表的尾部节点
+                DLinkedNode tail = removeTail();
+                // 删除哈希表中对应的项
+                map.remove(tail.key);
+            } else {
+                size++;
+            }
+            map.put(key, newNode);
+            addToHead(newNode);
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    private DLinkedNode removeTail() {
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+}
