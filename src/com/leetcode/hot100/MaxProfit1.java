bedd55377369cc0c3240d2ac0d68f72f5cb490b9
zj <565405576@qq.com>
2021-06-27 17:42:59 +0800

    买卖股票的最佳时机1

--- src/com/leetcode/hot100/MaxProfit1.java
+++ src/com/leetcode/hot100/MaxProfit1.java
@@ -16,6 +16,11 @@ package com.leetcode.hot100;
  * @author zhaojun
  */
 public class MaxProfit1 {
+    /**
+     * 时间O(n), 空间O(1)
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
         int result = 0;
         int min = prices[0];
@@ -24,5 +29,26 @@ public class MaxProfit1 {
             min = Math.min(min, prices[i]);
         }
         return result;
+    }
+
+    /**
+     * 时间O(n), 空间O(n)
+     * dp[i] 表示前i天的最大利润，因为我们始终要使利润最大化，则：
+     *  dp[i]=max(dp[i−1],prices[i]−minprice)
+     * @param prices
+     * @return
+     */
+    public int maxProfitDp(int[] prices) {
+        int[] dp = new int[prices.length];
+        dp[0]=0;
+        //minprice代表历史最低价格，
+        int minprice=prices[0];
+        // 所以i从1开始历史最低价格肯定是prices[0]
+        for (int i = 1; i < prices.length; i++) {
+            dp[i]=Math.max(dp[i-1],prices[i]-minprice);
+            minprice=Math.min(minprice,prices[i]);
+
+        }
+        return dp[prices.length-1];
     }
 }
