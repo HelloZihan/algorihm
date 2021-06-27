4fda556c15a92954c46fe9964bd2d40e14e70084
zj <565405576@qq.com>
2021-06-27 17:37:49 +0800

    桶排序

---
+++ src/com/jianzhi/offer/sort/BucketSort.java
@@ -0,0 +1,54 @@
+package com.jianzhi.offer.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * 桶排序
+ *
+ * https://blog.csdn.net/qq_27124771/article/details/87651495
+ *
+ * 复杂度：
+ * https://blog.csdn.net/zihonggege/article/details/104781491/
+ *
+ * @author zhaojun
+ */
+public class BucketSort {
+
+    public static void bucketSort(int[] arr){
+
+        // 计算最大值与最小值
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+
+        // 计算桶的数量
+        int bucketNum = (max - min) / arr.length + 1;
+        ArrayList<ArrayList<Integer>> bucketArr = new ArrayList<>(bucketNum);
+        for(int i = 0; i < bucketNum; i++){
+            bucketArr.add(new ArrayList<>());
+        }
+
+        // 将每个元素放入桶
+        for(int i = 0; i < arr.length; i++){
+            int num = (arr[i] - min) / (arr.length);
+            bucketArr.get(num).add(arr[i]);
+        }
+
+        // 对每个桶进行排序
+        for(int i = 0; i < bucketArr.size(); i++){
+            Collections.sort(bucketArr.get(i));
+        }
+
+        // 将桶中的元素赋值到原序列
+        int index = 0;
+        for(int i = 0; i < bucketArr.size(); i++){
+            for(int j = 0; j < bucketArr.get(i).size(); j++){
+                arr[index++] = bucketArr.get(i).get(j);
+            }
+        }
+    }
+}
