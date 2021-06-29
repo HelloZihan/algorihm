b83f1f07fab29f43ae6c077729113c14565b3a5e
zj <565405576@qq.com>
2021-06-29 21:54:29 +0800

    顺时针打印矩阵

--- src/com/jianzhi/offer/array/PrintMatrix.java
+++ src/com/jianzhi/offer/array/PrintMatrix.java
@@ -1,35 +1,39 @@
 package com.jianzhi.offer.array;
 
+import java.util.ArrayList;
+
 /**
- * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
- * 例如，如果输入如下4 X 4矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
- * 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+ * 剑指 Offer 29. 顺时针打印矩阵
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
  *
- * 示例1
- * 输入
- * [[1,2],[3,4]]
- * 返回值
- * [1,2,4,3]
+ * 示例 1：
+ *
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
  *
  * @author zhaojun
  */
 public class PrintMatrix {
 
-    public int moreThanHalfNum_Solution(int [] array) {
-        int count=0;
-        int target = array[0];
-        for (int a : array) {
-            if (count == 0) {
-                target = a;
-                count = 1;
-            } else {
-                if (a == target) {
-                    count++;
-                } else {
-                    count--;
-                }
-            }
+    /**
+     * https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/solution/mian-shi-ti-29-shun-shi-zhen-da-yin-ju-zhen-she-di/
+     * @param matrix
+     * @return
+     */
+    public int[] spiralOrder(int[][] matrix) {
+        if(matrix.length == 0) return new int[0];
+        int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+        int[] res = new int[(r + 1) * (b + 1)];
+        while(true) {
+            for(int i = l; i <= r; i++) res[x++] = matrix[t][i]; // left to right.
+            if(++t > b) break;
+            for(int i = t; i <= b; i++) res[x++] = matrix[i][r]; // top to bottom.
+            if(l > --r) break;
+            for(int i = r; i >= l; i--) res[x++] = matrix[b][i]; // right to left.
+            if(t > --b) break;
+            for(int i = b; i >= t; i--) res[x++] = matrix[i][l]; // bottom to top.
+            if(++l > r) break;
         }
-        return target;
+        return res;
     }
 }
