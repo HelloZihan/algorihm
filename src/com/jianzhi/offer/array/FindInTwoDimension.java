0b6cd4325c619a24f948a9b907e56221f192f1bb
zj <565405576@qq.com>
2021-06-29 21:33:14 +0800

    二维数组中的查找

--- src/com/jianzhi/offer/array/FindInTwoDimension.java
+++ src/com/jianzhi/offer/array/FindInTwoDimension.java
@@ -1,8 +1,12 @@
 package com.jianzhi.offer.array;
 
-/** todo 需要修改
- * 一个整型数组里除了两个数字只出现一次，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。 
- https://leetcode-cn.com/problems/single-number-iii/description/
+/**
+ *  二维数组中的查找
+ *
+ *  在一个二维数组中（每个一维数组的长度相同），
+ *  每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ *  请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ *
  * @author zhaojun
  */
 public class FindInTwoDimension {
