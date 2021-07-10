8f257843c3638ea3db894daca463bcc0a7aad970
zj <565405576@qq.com>
2021-07-10 23:23:19 +0800

    颜色分类(一次遍历)

--- src/com/leetcode/hot100/SortColors.java
+++ src/com/leetcode/hot100/SortColors.java
@@ -47,4 +47,50 @@ public class SortColors {
             }
         }
     }
+
+    public void sortColors1(int[] nums) {
+        int p0=0, p1=0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 1) {
+                int tmp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = tmp;
+                p1++;
+            } else if(nums[i] == 0) {
+                int tmp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = tmp;
+                if(p0 < p1) {
+                    tmp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = tmp;
+                }
+                p0++;
+                p1++;
+            }
+        }
+    }
+
+    /**
+     * p2往后移，p0往前移动
+     * @param nums
+     */
+    public void sortColors2(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p2 = n - 1;
+        for (int i = 0; i <= p2; ++i) {
+            while (i <= p2 && nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[p2];
+                nums[p2] = temp;
+                --p2;
+            }
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                ++p0;
+            }
+        }
+    }
 }
