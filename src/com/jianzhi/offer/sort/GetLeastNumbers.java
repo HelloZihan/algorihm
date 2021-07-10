144ac70e2d9097e7fc2ca4056c912be1b8d7573a
zj <565405576@qq.com>
2021-07-10 19:56:44 +0800

    最小的K个数(快排分段)

--- src/com/jianzhi/offer/sort/GetLeastNumbers.java
+++ src/com/jianzhi/offer/sort/GetLeastNumbers.java
@@ -8,9 +8,6 @@ import java.util.Arrays;
  * 给定一个数组，找出其中最小的K个数。例如数组元素是4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
  * 如果K>数组的长度，那么返回一个空的数组
  *
- * 此题可以看一下复杂度，不再是O(n)
- * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/3chong-jie-fa-miao-sha-topkkuai-pai-dui-er-cha-sou/
- *
  * @author zhaojun
  *
  * 熟悉快排
@@ -42,10 +39,49 @@ public class GetLeastNumbers {
         }
         a[i] = base;
 
-        quickSort(a,0,i-1);
+        quickSort(a,left,i-1);
         quickSort(a, j+1, right);
     }
 
+    /**
+     * 此题可以看一下复杂度，不再是O(n)
+     * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/3chong-jie-fa-miao-sha-topkkuai-pai-dui-er-cha-sou/
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k >= arr.length) {
+            return arr;
+        }
+        return quickSort(arr, k, 0, arr.length - 1);
+    }
+    private int[] quickSort(int[] arr, int k, int l, int r) {
+        int i = l, j = r;
+        while (i < j) {
+            while (i < j && arr[j] >= arr[l]) {
+                j--;
+            }
+            while (i < j && arr[i] <= arr[l]) {
+                i++;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, i, l);
+        if (i > k) {
+            return quickSort(arr, k, l, i - 1);
+        }
+        if (i < k) {
+            return quickSort(arr, k, i + 1, r);
+        }
+        return Arrays.copyOf(arr, k);
+    }
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5,6};
