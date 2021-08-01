5c94a24a6cb52cadce1a71caa13f35b3477ef8f6
zj <565405576@qq.com>
2021-08-01 19:18:47 +0800

    区域和检索(前缀和)

--- src/com/leetcode/hot100/NumArray.java
+++ src/com/leetcode/hot100/NumArray.java
@@ -10,7 +10,6 @@ package com.leetcode.hot100;
  * NumArray(int[] nums) 使用数组 nums 初始化对象
  * int sumRange(int i, int j) 返回数组 nums 从索引i到j（i≤j）范围内元素的总和，包含i、j两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
  *
- *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/range-sum-query-immutable
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
