9439cd5330914021b7a495d9b91a2921bbd1181a
zj <565405576@qq.com>
2021-08-01 19:18:05 +0800

    二维区域和检索

---
+++ src/com/leetcode/hot100/NumMatrix.java
@@ -0,0 +1,39 @@
+package com.leetcode.hot100;
+
+/**
+ * 304. 二维区域和检索 - 矩阵不可变
+ *
+ * 给定一个二维矩阵 matrix，以下类型的多个请求：
+ *
+ * 计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1,col1) ，右下角为 (row2,col2) 。
+ * 实现 NumMatrix 类：
+ *
+ * NumMatrix(int[][] matrix)给定整数矩阵 matrix 进行初始化
+ * int sumRegion(int row1, int col1, int row2, int col2)返回左上角 (row1,col1)、右下角(row2,col2)的子矩阵的元素总和。
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class NumMatrix {
+
+    int[][] sum;
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length+1;
+        int n = matrix[0].length+1;
+        sum = new int[m+1][n+1];
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2+1][col2+1] - sum[row1][col2+1] - sum[row2+1][col1] + sum[row1][col1];
+    }
+
+}
