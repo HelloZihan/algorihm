f07f3dd4c77829a71372a87091fc1550d7ad0eb0
zj <565405576@qq.com>
2021-09-14 17:57:56 +0800

    x 的平方根(二分法,注意int类型溢出)

---
+++ src/com/codetop/MySqrt.java
@@ -0,0 +1,48 @@
+package com.codetop;
+
+/**
+ * 69. x 的平方根
+ *
+ * 给你一个非负整数 x ，计算并返回x的平方根 。
+ * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+ * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ *
+ * 示例 1：
+ *
+ * 输入：x = 4
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：x = 8
+ * 输出：2
+ * 解释：8 的平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sqrtx
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaojun
+ */
+public class MySqrt {
+
+    /**
+     * 需要注意long类型，int类型46340的平方会达到int类型最大值，46341会溢出为负值，导致计算错误。
+     * @param x
+     * @return
+     */
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            //注意此处long类型，否则会溢出导致算错
+            if ((long)mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+}
